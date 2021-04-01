67efc3817cba4de5fbb20ad8e2a689597398b65d
SirTenzin <jeffreypfc21@gmail.com>
2021-04-01 20:14:03 +0100

    Auto Commit by YARN

--- src/modules/Message.js
+++ src/modules/Message.js
@@ -5,20 +5,29 @@ export default class Message extends React.Component {
     constructor() {
         super()
         this.state = {
-            message: 'Welcome vistor'
+            message: 'Welcome vistor',
+            isSubbed: true
         }
     }
  
     changeMessage() {
-        this.setState({
-            message: "Welcome subsciber"
-        })
+        if(this.state.isSubbed) {
+            this.setState({
+                message: "Welcome random person i dont know",
+                isSubbed: false
+            })
+        } else {
+            this.setState({
+                message: "Welcome subsciber i defo know",
+                isSubbed: true
+            })
+        }
     }
     render() {
         return (
          <div>
             <h1><code>Hey I'm Tenzin, Welcome to tenzin.land, {this.state.message}</code></h1>
-            <button onClick={() => this.changeMessage()}>Subscibe</button>
+            <button onClick={() => this.changeMessage()}>{this.state.isSubbed}</button>
         </div>
         )
     }
