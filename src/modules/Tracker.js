ca6602901354a88cc19315cf31323c7bcc0a77e7
SirTenzin <jeffreypfc21@gmail.com>
2021-09-14 14:47:07 -0500

    Auto Commit by YARN

--- src/modules/Tracker.js
+++ src/modules/Tracker.js
@@ -1,10 +1,6 @@
 import React, { useState } from "react";
-import '../Tracker.css';
-import '../App.css';
-
-import SearchIcon from "@material-ui/icons/Search";
-import CloseIcon from "@material-ui/icons/Cancel";
-
+import "../Tracker.css";
+import "../App.css";
 
 function Tracker({ placeholder, data }) {
   const [filteredData, setFilteredData] = useState([]);
@@ -24,49 +20,46 @@ function Tracker({ placeholder, data }) {
     }
   };
 
-  const clearInput = () => {
-    setFilteredData([]);
-    setWordEntered("");
-  };
-
-  if(document.querySelector("body > nav")) {
-    var el = document.querySelector("body > nav")
-    el.style.visibility = 'hiddenn';
-    el.style.display = 'none';
-   }
+  if (document.querySelector("body > nav")) {
+    var el = document.querySelector("body > nav");
+    el.style.visibility = "hiddenn";
+    el.style.display = "none";
+  }
 
   return (
     <div className="App">
-        <div className="App-header">
+      <div className="App-header">
         <div className="search">
-      <div className="searchInputs App">
-        <input
-          type="text"
-          placeholder={placeholder}
-          value={wordEntered}
-          onChange={handleFilter}
-        />
-        {/* <div className="searchIcon">
-          {filteredData.length === 0 ? (
-            <SearchIcon />
-          ) : (
-            <CloseIcon id="clearBtn" onClick={clearInput} />
+          <div className="searchInputs App">
+            <input
+              type="text"
+              placeholder={placeholder}
+              value={wordEntered}
+              onChange={handleFilter}
+            />
+          </div>
+          {filteredData.length !== 0 && (
+            <div className="dataResult App">
+              {filteredData
+                .slice(0, 15)
+                .sort((a, b) => (Number(a.price) > Number(b.price) ? 1 : -1))
+                .map((value, key) => {
+                  return (
+                    <a
+                      className="dataItem App"
+                      href={value.link}
+                      target="_blank"
+                    >
+                      <p style={{ textAlign: "center" }}>
+                        {value.shop} - {value.name} (${value.price})
+                      </p>
+                    </a>
+                  );
+                })}
+            </div>
           )}
-        </div> */}
+        </div>
       </div>
-      {filteredData.length != 0 && (
-        <div className="dataResult App">
-          {filteredData.slice(0, 15).sort((a, b) => Number(a.price) > Number(b.price) ? 1 : -1).map((value, key) => {
-            return (
-              <a className="dataItem App" href={value.link} target="_blank">
-                <p style={{textAlign: "center"}}>{value.shop} - {value.name}  (${value.price})</p>
-              </a>
-            );
-          })}
-        </div>
-      )}
-    </div>
-        </div>
     </div>
   );
 }
